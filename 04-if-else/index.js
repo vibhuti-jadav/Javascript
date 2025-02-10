9500ce6acb783e51aff82ee5eb90ec22e1866120
vibhuti-jadav <vibhutij109@gmail.com>
2025-02-10 18:18:44 +0530

    added

---
+++ 04-if-else/index.js
@@ -0,0 +1,34 @@
+// let a = 45;
+// let b = 40;
+// let c = 40;
+
+// if(a==b && b==c){
+//     console.log("all are equal")
+// }
+// else if(a>=b && a>=c)
+// {
+//      console.log("a is greater")
+// }
+// else if(b>=a && b>=c)
+// {
+//     console.log("b is greater")
+// }
+// else{
+//     console.log("c is greter")
+// }
+
+// first();
+// function first(){
+//     console.log("first one")
+//     second()
+// }
+
+// function second(){
+//     console.log("second one")
+//     third()
+// }
+
+function third(){
+    console.log("third one")
+}
+
