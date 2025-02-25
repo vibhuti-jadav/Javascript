06e21efcb2e7b9e314eb2f89afc2cb413837f1b7
vibhuti-jadav <vibhutij109@gmail.com>
2025-02-25 15:34:55 +0530

    how-js-work

---
+++ 15-work/index.js
@@ -0,0 +1,28 @@
+
+let val1 = 20;
+let val2 = 30;
+// function square(n){
+//     let result = n*n;
+//     return result;
+// }
+
+function fun(num1, num2){
+    let result = num1 + num2  
+    // fun2();
+    return result;
+}
+
+// function fun2(){
+//     console.log("this isfun2 ")
+// }
+
+
+
+
+let result1 = fun(val1, val2);
+let result2 = fun(50, 60);
+
+console.log(fun())
+
+// let result3 = square(5)
+
