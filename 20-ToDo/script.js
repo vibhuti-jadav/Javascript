346df46fe6c24221bc448ed01b0c0658daa0bb45
vibhuti-jadav <vibhutij109@gmail.com>
2025-03-05 16:06:04 +0530

    To-Do

---
+++ 20-ToDo/script.js
@@ -0,0 +1,87 @@
+
+let task = document.getElementById("task")
+let add = document.getElementById("add")
+let tbody = document.getElementById("tbody")
+
+
+add.addEventListener("click", addTask)
+
+
+let task_arr = [];
+
+
+function addTask(){
+
+    let obj = {
+        text : task.value,
+        status : false
+    }
+
+    task_arr.push(obj)
+
+   console.log(task_arr)
+
+    showTask(task_arr)
+
+}
+
+
+function changeStatus(e){
+   let new_task_arr = task_arr.map((ele) => {
+            if(ele.text == e)
+            {     
+                ele.status = !ele.status
+            }
+
+            return ele
+            
+    })
+
+
+showTask(new_task_arr)
+    
+}
+
+function showTask(array){
+
+    tbody.innerHTML = ""
+    array.map((ele) => { 
+
+           let tr = document.createElement("tr");
+
+            let td_text = document.createElement("td"); 
+            let td_status = document.createElement("td"); 
+            let td_edit = document.createElement("td"); 
+            let td_delete = document.createElement("td"); 
+            let btn_edit = document.createElement("button"); 
+            let btn_delete = document.createElement("button"); 
+
+            td_text.textContent = ele.text;
+
+            td_status.textContent = ele.status ? "Completed" : "not completed";
+
+            td_status.onclick = () => changeStatus(ele.text)
+
+            btn_edit.textContent="🖊️"
+            btn_delete.textContent="🗑️"
+
+            td_edit.append(btn_edit)
+
+            td_delete.append(btn_delete)
+
+
+             tr.append(td_text, td_status, td_edit, td_delete)
+
+             if(ele.status)
+             {
+                 tr.setAttribute("class", 'table-success')
+             }
+             else{
+                tr.setAttribute("class", 'table-danger')
+             }
+                    
+        tbody.append(tr)         
+   })
+
+}
+
