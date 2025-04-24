0b165f76ad1836f98969ff70f7058761376333e1
vibhuti-jadav <vibhutij109@gmail.com>
2025-04-24 17:28:00 +0530

    json-server

--- 38-component/fetchapi.js
+++ 38-component/fetchapi.js
@@ -1,8 +1,34 @@
-const fetchapiData = async (url) => {
-    let resp  =  await fetch(url)
-    let data  =  await resp.json()
+// const fetchapiData = async (url) => {
+//     let resp  =  await fetch(url)
+//     let data  =  await resp.json()
 
-    return data
-}
+//     return data
+// }
 
-export default fetchapiData
+// // export default fetchapiData
+
+const fetchapi = async (url) =>{
+    let loading = true;
+    let error = false;
+    let data =[]
+        try{
+               let resp = await fetch(url)
+    
+             data = await resp.json()
+        }
+        catch{
+            error = false
+          
+        }
+        finally{
+            loading = false
+        }
+        
+    
+        return {loading, error, data}
+    }
+    
+       
+    
+    
+    export default fetchapi
